6a2c751b5cfd293e89a1f743de4bce137608e456
jhonlopera2006 <77785841+jhonlopera2006@users.noreply.github.com>
2021-03-25 20:24:29 -0500

    Add files via upload

---
+++ actividad2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+#include <windows.h> 
+#include <time.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+main(){
+	int suma,peso,numero;
+	cout<<"++++++++++++++++ \n";
+	cout<<"+ hola a todos + \n";
+	cout<<"+ 1.saludar    + \n";
+	cout<<"+ 2.despedir   + \n";
+	cout<<"+ 3.insultar           + \n";
+	cout<<"+ 4.salir       + \n";
+	cout<<"++++++++++++++++ \n";
+	cout<<"eliges una opcion para seguir "
+	sleep(1000);
+	system("cls");
+	cin>>peso
+	
+	if(peso==1){
+	cout"hola, como estas";
+	sleep(1000);
+	system("cls")
+		}
+	
+
+	
+	
+}
---
+++ parcial.cpp
@@ -0,0 +1,73 @@
+#include<iostream> 
+using namespace std; 
+#include<conio.h> 
+#include <windows.h> 
+#include <time.h> 
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<string>
+
+main(){
+	
+	int chocolates=500,choc,cho, doritos=1000,dor,do, papas=1500,pa,p,precio;
+	
+	cout<<"hola muy buenas como esta sea vienvenid@ a nuestra tienda \n";
+	cout<<"en nuestra tienda vendemos chocolates \n";
+	cout<<"tambien vendemos doritos \n";
+	cout<<"y por ultimo vendemos papas ";
+     cout<<"los chocolates tienen un precio de: ";
+     cout<<chocolates;
+     cout<<"\n";
+     cout<<"los doritos tienen un precio de: ";
+     cout<<doritos;
+     cout<<"\n";
+     cout<<"la papas tienen un precio de: ";
+     cout<<papas;
+     cho=choc*500;
+     do=dor*1000;
+	p=pa*1500
+	cout<<"una pregunta \n";
+	cout<<"cuantoa chocolates desea llevar ";
+	cout<<"\n";
+	cin>>choco
+	cout<<"cuantos doritos desea llevar ";
+	cout<<"\n";
+	cin>>dori
+	cout<<"cuantas papas desea llevar ";
+	cout<<"\n";
+	cin>>pap
+	cout<<"y el valor total es ";
+	suma =cho+do+p;
+	cout<<suma ;
+	cout<<"\n";	
+	if(precio=suma){
+		cout<<"gracias por su compra ";
+		cout<<"que tenga un buen dia ";
+	}
+	else	
+	{
+	cout<<"no tiene sufuciente dinero para pagar su compra  ";	
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
